f7afbbc717880ed0f2962bac278191513852d5bc
Aj <adtune6509@student.waynecc.edu>
2018-08-13 22:56:26 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
-const __token__ = "SET BOT TOKEN HERE"
+const __token__ = "SET BOT TOKEN HERE";
+const __PORT__ = "SET PORT HERE";
 exports.token = (token) => {
     return token ? token : __token__;
-}
-exports.command_prefix = "~"
+};
+exports.command_prefix = "~";
+exports.PORT = (typeof __PORT__ == "number") ? __PORT__ : 3000;
