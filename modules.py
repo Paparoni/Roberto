7c575a3bbe7a491d6d450bf53917b6b53e273182
Aj <adtune6509@student.waynecc.edu>
2018-08-17 22:30:04 -0400

    Update modules.py

--- modules.py
+++ modules.py
@@ -1,10 +1,19 @@
 import subprocess
 import sys
+import os
 modules = ['requests', 'Pillow']
 for module in modules:
     print('success')
     if module in sys.modules is False:
         print('Installing {}'.format(module))
         subprocess.call([sys.executable, '-m', 'pip', 'install', module])
+
+if os.path.isfile('./roberto.bat') == False:
+    cwd = os.getcwd()
+    bat = open("roberto.bat","w+")
+    bat.write("cd {}\n".format(cwd))
+    bat.write("node roberto.js")
+    bat.close()
+
 sys.stdout.flush()
 
