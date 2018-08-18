6cec56a629d94d45f14c7cacbca5bc5456fc3174
Aj <adtune6509@student.waynecc.edu>
2018-08-17 22:31:52 -0400

    Create sparrow.js

---
+++ commands/sparrow.js
@@ -0,0 +1,10 @@
+// Draws user avatar in a jack sparrow meme using python.
+const spawn = require("child_process").spawn;
+exports.execute = (Roberto, message, params, success, error) => {
+    let user = message.author
+    let url = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`;
+    drawScript = spawn('py', ['draw.py', 'sparrow', url]);
+    drawScript.stdout.on('data', (data) => {
+        message.channel.send('', { files: ['./images/out.png'] });
+    })
+}
