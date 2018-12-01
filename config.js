6ae427954c198eec48ee474c9bc9e2aed98773a2
Aj <antwauntune25@wcpsstudent.org>
2018-12-01 08:57:44 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const __token__ = "NDc2OTQzMDcxMzcyOTAyNDIw.DqGq2w.uOv4ApoZSe3Nkk0X8Z5nRHRSAJI";
+const __token__ = "";
 const __PORT__ = process.env.PORT;
 exports.token = (token) => {
     return token ? token : __token__;
