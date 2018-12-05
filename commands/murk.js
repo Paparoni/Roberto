31b6789b139ed00f3d44ef4177d417298891bbde
Aj <adtune6509@student.waynecc.edu>
2018-12-04 21:08:46 -0500

    Update murk.js

--- commands/murk.js
+++ commands/murk.js
@@ -1,9 +1,9 @@
 // Distorts the user image
 const spawn = require("child_process").spawn;
 exports.execute = (Roberto, message, params, success, error) => {
-    let user = message.mentions[0] || message.author
+    let user = message.mentions.members.first().user || message.author
     let url = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`;
-    drawScript = spawn('python', ['draw.py', 'distort', url]);
+    drawScript = spawn('py', ['draw.py', 'distort', url]);
     drawScript.stdout.on('data', (data) => {
         message.channel.send('', { files: ['./images/out-trans.png'] });
     })
